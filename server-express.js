60ceb6441e1e15fdd54f0140ea51f85ba74c063c
maciejszulia <maciej.szulia@gmail.com>
2022-06-02 16:18:41 +0200

    hbs + express first steps

--- server-express.js
+++ server-express.js
@@ -1,11 +1,30 @@
 const express = require("express")
 const port = 3001
 
+const generateTitle = () => {
+    var pageTitle = 'Node js'
+    console.log('changed page title: ', pageTitle)
+    return pageTitle
+}
+
+const generateBody = () => {
+    var pageBody = 'Hello world!!'
+    console.log('changed page body: ', pageBody)
+    return pageBody
+}
+
 const app = express()
 app.set('view engine', 'hbs')
 
 app.get('/', function (req, res) {
-    res.send('Hello express')
+
+    const title = generateTitle()
+    const body = generateBody()
+
+    res.render('index', {
+        pageTitle: title,
+        pageBody: body
+    })
 })
 
 app.get('/kontakt', function (req, res) {
