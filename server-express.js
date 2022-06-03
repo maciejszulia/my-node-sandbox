10e5110f70628c2ad1d866c6ca8ad9fa5686ad4c
maciejszulia <maciej.szulia@gmail.com>
2022-06-03 14:09:48 +0200

    Added .gitignore

--- server-express.js
+++ server-express.js
@@ -1,34 +1,25 @@
 const express = require("express")
+const setFunctions = require("./setFunctions.js")
 const port = 3001
 
-const generateTitle = () => {
-    var pageTitle = 'Node js'
-    console.log('changed page title: ', pageTitle)
-    return pageTitle
-}
-
-const generateBody = () => {
-    var pageBody = 'Hello world!!'
-    console.log('changed page body: ', pageBody)
-    return pageBody
-}
 
 const app = express()
 app.set('view engine', 'hbs')
 
-app.get('/', function (req, res) {
+app.get('/', function (request, response) {
 
-    const title = generateTitle()
-    const body = generateBody()
 
-    res.render('index', {
+    const title = setFunctions.generateTitle()
+    const body = setFunctions.generateBody()
+
+    response.render('index', {
         pageTitle: title,
         pageBody: body
     })
 })
 
-app.get('/kontakt', function (req, res) {
-    res.send('Catch me at maciej.szulia@gmail.com')
+app.get('/kontakt', function (request, response) {
+    response.send('Catch me at maciej.szulia@gmail.com')
 })
 
 app.listen(port)
---
+++ setFunctions.js
@@ -0,0 +1,16 @@
+const generateTitle = () => {
+    var pageTitle = 'Node js'
+    return pageTitle
+}
+
+const generateBody = () => {
+    var pageBody = 'Hello world!!'
+    return pageBody
+}
+
+console.log("loaded")
+
+module.exports = {
+    generateTitle,
+    generateBody
+}
